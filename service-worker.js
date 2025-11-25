04a9409405d78c8fe6b829a4d08f509dafb631e6
akln55 <118135868+akln55@users.noreply.github.com>
2025-11-25 20:23:00 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
-self.addEventListener("install", () => {
+self.addEventListener('install', evt => {
   console.log("Service worker yüklendi");
+});
+
+self.addEventListener('activate', evt => {
+  console.log("Service worker aktif");
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(fetch(evt.request));
 });
